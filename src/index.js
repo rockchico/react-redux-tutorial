1a18ed83d477b2998e0ffe801562320caaacdcfd
Francisco Schwertner <rockchico@gmail.com>
2019-12-10 11:59:57 -0300

    Redux: Actions

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import store from './store';
+import { STORY_ARCHIVE } from './constants/actionTypes';
 import * as serviceWorker from './serviceWorker';
 
 
 
 ReactDOM.render(
-    <App stories={store.getState()} onArchive={() => {}} />,
+    <App 
+      stories={store.getState().storyState}
+      onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })}
+    />,
     document.getElementById('root')
 );
 
