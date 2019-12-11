f8e38be15a675e1a944fee249d446f771e99123c
Francisco Schwertner <rockchico@gmail.com>
2019-12-11 16:01:47 -0300

    teste messages

---
+++ src/api/ApiMessages.js
@@ -0,0 +1,52 @@
+// api
+const BASE_URL = 'http://localhost:3000/messages';
+
+const ApiAddMessage = (id, message) => {
+  return fetch(`${BASE_URL}/${id}`, {
+    method: 'POST',
+    body: JSON.stringify(message)
+  })
+  .then(res => res.json())
+  //.catch(err => console.log(err));
+}
+
+const ApiUpdateMessage = (id, message) => {
+  return fetch(`${BASE_URL}/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify(message)
+  })
+  .then(res => res.json())
+  //.catch(err => console.log(err));
+}
+
+const ApiDeleteMessage = (params) => {
+  
+  //console.log(params.id)
+
+  return fetch(`${BASE_URL}/${params.id}`, {
+      method: 'DELETE'
+  })
+  .then(res => res.json())
+  //.catch(err => console.log(err));
+}
+
+const ApiGetMessages = () => {
+  
+  //console.log(params.id)
+
+  return fetch(`${BASE_URL}/`, {
+      method: 'GET'
+  })
+  .then(res => res.json())
+  //.catch(err => console.log(err));
+}
+
+
+
+// fim api
+export {
+  ApiAddMessage,
+  ApiUpdateMessage,
+  ApiDeleteMessage,
+  ApiGetMessages
+};
--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -4,3 +4,21 @@ export const STORIES_FETCH_ERROR = 'STORIES_FETCH_ERROR';
 export const STORIES_ADD = 'STORIES_ADD';
 
 export const STORY_ADD = 'STORY_ADD';
+
+
+// MESSAGES API
+export const MESSAGE_ADD                    = 'MESSAGE_ADD';
+export const MESSAGE_API_ADD                = 'MESSAGE_API_ADD';
+export const MESSAGE_API_ADD_ERROR          = 'MESSAGE_API_ADD_ERROR';
+
+export const MESSAGE_UPDATE                 = 'MESSAGE_UPDATE';
+export const MESSAGE_API_UPDATE             = 'MESSAGE_API_UPDATE';
+export const MESSAGE_API_UPDATE_ERROR       = 'MESSAGE_API_UPDATE_ERROR';
+
+export const MESSAGE_DELETE                 = 'MESSAGE_DELETE';
+export const MESSAGE_API_DELETE             = 'MESSAGE_API_DELETE';
+export const MESSAGE_API_DELETE_ERROR       = 'MESSAGE_API_DELETE_ERROR';
+
+export const MESSAGE_FETCHALL               = 'MESSAGE_FETCHALL';
+export const MESSAGE_API_FETCHALL           = 'MESSAGE_API_FETCHALL';
+export const MESSAGE_API_FETCHALL_ERROR     = 'MESSAGE_API_FETCHALL_ERROR';
